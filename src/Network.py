5b0cd41044d4fea87e1f930a939df8a99fa53e19
FPuoti255 <fpuoti99@gmail.com>
2022-08-11 13:04:53 +0200

    starting live edge graph algorithm

--- src/Network.py
+++ src/Network.py
@@ -1,23 +1,20 @@
-import numpy as np
-from .Node import *
-import random
 import networkx as nx
+import numpy as np
+import random
 import matplotlib as plt
+
+from Constants import *
 
 seed = 1234
 
 class Network:
 
-    def __init__(self, n_nodes : int, fully_connected = False, alternative_formulation = False):
+    def __init__(self, fully_connected = False, nodes_activation_probabilities = np.random.uniform(size=NUM_OF_PRODUCTS)):
 
         self.graph = nx.DiGraph()
-
-        nodes = []
-        for id in range(n_nodes):
-          nodes.append(Node(id, random.random()*100, random.random() if alternative_formulation else None))
         
-        adjacency_matrix = np.round(np.random.uniform(low=0.01, high = 1.000001, size = (n_nodes, n_nodes)), 2)
-        adjacency_matrix[np.diag_indices(n = n_nodes, ndim = 2)] = 0.0
+        adjacency_matrix = np.random.uniform(low=0.01, high = 1.000001, size = (NUM_OF_PRODUCTS, NUM_OF_PRODUCTS))
+        adjacency_matrix[np.diag_indices(n = NUM_OF_PRODUCTS, ndim = 2)] = 0.0
 
         if not fully_connected:
             indices = np.argwhere(adjacency_matrix).T
@@ -25,22 +22,31 @@ class Network:
             random_indices = indices[:, random_indices]
             adjacency_matrix[tuple(random_indices)] = 0.0
         
-        if alternative_formulation :
-            for i in range(n_nodes):
-                adjacency_matrix[:, i] = np.round( adjacency_matrix[:, i] / np.sum(adjacency_matrix[:, i]), 2)
+        for i in range(NUM_OF_PRODUCTS):
+            col_sum = np.sum(adjacency_matrix[:, i])
+            if col_sum != 0 :
+                adjacency_matrix[:, i] = adjacency_matrix[:, i] / col_sum
 
-        #self.graph.add_nodes_from(nodes)
-        for i in range(n_nodes):
-            for j in range(n_nodes):
+        adjacency_matrix = np.round(adjacency_matrix, 2)
+
+        for id in range(NUM_OF_PRODUCTS):
+            self.graph.add_node(id, margin = round(random.random()*100, 2), activation_threshold =  round(nodes_activation_probabilities[id], 2) )
+
+        for i in range(NUM_OF_PRODUCTS):
+            for j in range(NUM_OF_PRODUCTS):
                 if(adjacency_matrix[i][j] != 0):
-                    self.graph.add_edge(nodes[i], nodes[j], weight = adjacency_matrix[i][j])
+                    self.graph.add_edge(i, j, weight = adjacency_matrix[i][j])
 
 
+    def get_random_seeds(self) -> np.array:
+        n = random.randint(1,2)
+        nodes = np.arange(NUM_OF_PRODUCTS)
+        np.random.shuffle(nodes)
+        return np.isin(np.arange(NUM_OF_PRODUCTS), nodes[:n])
     
     def generate_live_edge_graph():
-        live_edges = []
-        self.live_edges_adjacency_matrix = np.zeros_like(self.adjacency_matrix)
-        self.edges = np.argwhere(self.adjacency_matrix)
+        seeds = self.get_random_seeds()
+
 
     def montecarlo_estimation():
         pass
--- src/Node.py
+++
@@ -1,5 +0,0 @@
-class Node:
-    def __init__(self, id : int, margin : float, activation_threshold = None):
-        self.node_id = id
-        self.margin = margin # profit when selling one unit of the product
-        self.activation_threshold = activation_threshold
