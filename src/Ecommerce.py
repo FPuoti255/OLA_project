3cdb8cebf293ce59c1f04c101b196876f4f0d5d4
FPuoti255 <fpuoti99@gmail.com>
2022-08-31 18:10:55 +0200

    step-2 corrected

--- src/Ecommerce.py
+++ src/Ecommerce.py
@@ -5,21 +5,30 @@ from Constants import *
 from Environment import Environment
 import random
 
+
 class Ecommerce(object):
     def __init__(self, B_cap : float):
         self.B_cap = B_cap
         self.budgets = np.arange(start = 0, stop = self.B_cap+1, step = self.B_cap/10)
         
         # For very campaign, you can imagine a maximum expected value of 𝛼_i (say 𝛼_i_bar)
-        self.alpha_bars = [0.6, 0.75, 0.85, 0.57, 1]
+        # In order to generate an array of 𝛼_i_bar with sum equal to 1, we used a multinomial distribution.
+        # Notice that we needed to include also the 'competitors product', and we decided to give to all the products equal probability -> [1 / (NUM_OF_PRODUCTS+1)]
+        # the 𝛼_0 is the one corresponding to the competitors product
+        self.alpha_bars = np.random.multinomial(100, [1/(NUM_OF_PRODUCTS+1)] * (NUM_OF_PRODUCTS+1))/100
 
-        # The higher the scale parameter, the slower will be the convergence to the 𝛼_i_bar (the smoother the function)
-        self.scale_params = [62, 32, 25 , 53, 40]
-        
+        self.functions_dict = [
+            lambda x : x /np.sqrt( 1 + x**2) * self.alpha_bars[1] if x < self.B_cap * 0.7 else self.alpha_bars[1] ,
+            lambda x : np.tanh(x) * self.alpha_bars[2] if x < self.B_cap * 0.45 else self.alpha_bars[2] ,
+            lambda x : x / (1 + x) * self.alpha_bars[3] if x < self.B_cap * 0.6 else self.alpha_bars[3] ,
+            lambda x : 2/np.pi * np.arctan(np.pi / 2 * x) * self.alpha_bars[4] if x < self.B_cap * 0.5 else self.alpha_bars[4] ,
+            lambda x : x * self.alpha_bars[5] if x < self.B_cap * 0.8 else self.alpha_bars[5] 
+        ]
 
+    
     # Function used to map from the budget domain to the alpha domain
     def mapping_function (self, budget : float, prod_id : int):
-        return min(self.alpha_bars[prod_id], 0.1 + np.tanh(budget/(2*self.scale_params[prod_id])))
+        return self.functions_dict[prod_id](budget/self.B_cap)
 
 
     def dynamic_algorithm(self, table : np.ndarray):
@@ -49,15 +58,21 @@ class Ecommerce(object):
         return budgets_index[::-1]
 
     def solve_optimization_problem(self, env : Environment, nodes_activation_probabilities):
-
+        '''
+        The algorithm returns the best budget allocation for each product
+        '''
         value_per_click = np.dot(nodes_activation_probabilities, env.get_product_prices().T)
 
-        matrix = np.array([[self.mapping_function(budget = bu, prod_id = prd) * value_per_click[prd] for bu in self.budgets] for prd in range(NUM_OF_PRODUCTS)])
-        budgets_indexes = self.dynamic_algorithm(table = matrix)
-        print('-------MATRIX---------')
-        print(matrix)
-        print('--------BUDGETS INDEXES---------')
-        print(budgets_indexes)
-        print('--------BUDGETS ALLOCATION---------')
-        print(self.budgets[budgets_indexes])
+        exp_num_clicks = np.array([[self.mapping_function(budget = bu, prod_id = prd) for bu in self.budgets] for prd in range(NUM_OF_PRODUCTS)])
 
+        matrix = np.array([exp_num_clicks[i, :] * value_per_click[i] for i in range(NUM_OF_PRODUCTS)])
+        print(exp_num_clicks)
+        budgets_indexes = self.dynamic_algorithm(table = matrix)
+        # print('-------MATRIX---------')
+        # print(matrix)
+        # print('--------BUDGETS INDEXES---------')
+        # print(budgets_indexes)
+        # print('--------BUDGETS ALLOCATION---------')
+        # print(self.budgets[budgets_indexes])
+
+        return self.budgets[budgets_indexes]
