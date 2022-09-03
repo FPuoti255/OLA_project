e6e6426eac93bd0bc98d074b284562fc697ade53
FPuoti255 <fpuoti99@gmail.com>
2022-09-03 19:57:15 +0200

    step 3 implementation GP-TS (maybe) completed. It last the GP - UCB1 implementation

--- src/Ecommerce_step3.py
+++ src/Ecommerce_step3.py
@@ -15,34 +15,43 @@ class Ecommerce_step3(Ecommerce):
         self.n_arms = self.budgets.shape[0]
 
         self.t = 0
-        self.rewards_per_arm = x =[[] for i in range(self.n_arms)]
-        self.collected_rewards = np.array([])
 
-        self.means = np.ones(self.n_arms) * 0.5
-        self.sigmas = np.ones(self.n_arms)* 0.4
+        # https://stats.stackexchange.com/a/316088
+        # I'm generating a distribution of the budgets for each product
+        self.means = np.ones(shape = (NUM_OF_PRODUCTS, self.n_arms)) * 0.5
+        self.sigmas = np.ones(shape = (NUM_OF_PRODUCTS, self.n_arms)) * 2
 
-        self.pulled_arms = []
+        self.pulled_arms = [[] for i in range(NUM_OF_PRODUCTS)]
+        self.pulled_arms_idxs = [[] for i in range(NUM_OF_PRODUCTS)]
+
+        self.rewards_per_arm = [[[] for i in range(self.n_arms)] for j in range(NUM_OF_PRODUCTS) ]
+        self.collected_rewards = [[] for i in range(NUM_OF_PRODUCTS)]
+
 
         alpha = 10.0
         kernel = C(1.0, (1e-3, 1e3)) * RBF(1.0, (1e-3, 1e3))
-        self.gp = GaussianProcessRegressor(kernel = kernel, alpha = alpha, 
-                                    normalize_y = True, n_restarts_optimizer = 9)
 
+        # we need one gaussian regressor for each product
+        self.gaussian_regressors = [ GaussianProcessRegressor(kernel = kernel, alpha = alpha, 
+                                    normalize_y = True, n_restarts_optimizer = 9) for i in range(NUM_OF_PRODUCTS)]
 
     def update_observations(self, pulled_arm, reward):
-        for i in range(pulled_arm.shape[0]):
-            self.rewards_per_arm[int(np.where(self.budgets == pulled_arm[i])[0])].append(reward[i])
-            self.pulled_arms.append(pulled_arm[i])
 
-        self.collected_rewards = np.append(self.collected_rewards, reward)
-        
+        for i in range(NUM_OF_PRODUCTS):
+            self.rewards_per_arm[i][int(np.where(self.budgets == pulled_arm[i])[0])].append(reward[i])
+            self.pulled_arms[i].append(pulled_arm[i])
+            self.collected_rewards[i].append(reward[i])
+
 
     def update_model(self):
-        x = np.atleast_2d(self.pulled_arms).T
-        y = self.collected_rewards
-        self.gp.fit(x, y)
-        self.means, self.sigmas = self.gp.predict(np.atleast_2d(self.budgets).T, return_std=True)
-        self.sigmas = np.maximum(self.sigmas, 1e-2)
+        for i in range(NUM_OF_PRODUCTS):
+            x = np.atleast_2d(self.pulled_arms[i]).T
+            y = np.array(self.collected_rewards[i])
+            self.gaussian_regressors[i].fit(x, y)
+
+            self.means[i], self.sigmas[i] = self.gaussian_regressors[i].predict(X = np.atleast_2d(self.budgets).T, return_std=True)
+            self.sigmas[i] = np.maximum(self.sigmas[i], 1e-2)
+        
 
     def update(self, pulled_arm, reward):
         self.t += 1
@@ -50,37 +59,13 @@ class Ecommerce_step3(Ecommerce):
         self.update_model()
 
     def pull_arm(self, nodes_activation_probabilities):
-        sampled_values = np.random.normal(loc=self.means, scale=self.sigmas)
+        a = np.multiply(self.means, self.sigmas)
+        b = np.multiply(self.sigmas, (1 - self.means))
 
-        sampled_values[np.where(sampled_values < 0)] = 0
-        sampled_values[np.where(sampled_values >= 1)] = 0.99
-
-        value_per_click = np.dot(nodes_activation_probabilities, self.product_prices.T)
+        samples = np.random.beta(a = a, b = b)
+        max_for_each_product = samples.max(axis=1) #axis = 1 means row-wise
         
-        exp_reward = np.zeros(shape = (NUM_OF_PRODUCTS, self.budgets.shape[0]))
+        # np.random.choice because it may happen to have more than one value
+        arms_idx_for_each_product = [np.random.choice(np.where(samples[i, :] == max_for_each_product[i])[0]) for i in range(NUM_OF_PRODUCTS)]
 
-        for prd in range(NUM_OF_PRODUCTS):
-            for j in range(1, self.n_arms):
-                exp_reward[prd][j] = value_per_click[prd] * sampled_values[j]
-        
-        superarm_idx, _ = self.dynamic_algorithm(table = exp_reward)
-
-        return sampled_values[superarm_idx], superarm_idx
-    
-    def dynamic_algorithm(self, table):
-        return super().dynamic_algorithm(table)
-
-
-    def generate_superarms(self):
-        # generating all the possible combination with replacement of 5 (campaigns) 
-        # over the 8 possible budgets
-        combinations = np.array([comb for comb in combinations_with_replacement(self.budgets, 5) if np.sum(comb) <= self.B_cap], dtype=float)
-
-        # the combinations do not have any order, thus using the permutation we consider
-        # all the possible assignment of those budgets to a given campaign
-        perms = []
-        for comb in combinations:
-            [perms.append(perm) for perm in permutations(comb)]
-        perms = np.array(list(set(perms))) #set() to remove duplicates
-
-        return perms
+        return self.budgets[arms_idx_for_each_product]
